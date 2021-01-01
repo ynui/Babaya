4c2429a258698ce820b5e6f7d9e0757f5ab2ca12
Ynui <ynui12@gmail.com>
2021-01-01 23:40:55 +0200

    landing page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname+'/login.html'));
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 module.exports = router;
